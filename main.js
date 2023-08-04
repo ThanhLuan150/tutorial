10cb6b7906d6c2d6868ce28606b594e7fb0ec806
ThanhLuan150 <116501650+ThanhLuan150@users.noreply.github.com>
2023-08-04 08:19:20 +0700

    tutorial

---
+++ main.js
@@ -0,0 +1,474 @@
+// // var Name ='Luan';
+// // var FullName="Lê Trương Thành Luan";
+// // var age='20 tuổi';
+// // var address= '101B Lê Hữu Trác, Phước Mỹ , Sơn Trà, Đà Nẵng'
+
+// // alert(Name);
+// // alert(FullName);
+// // alert(age);
+// // alert(address);
+
+// // console.log(Name);
+// // console.log(FullName);
+// // console.log(age);
+// // console.log(address)
+
+// // confirm(Name);
+// // prompt('Nhập thông tin cá nhân bạn vào nhé!')
+// // setTimeout(function(){
+// //     alert('Như quần què')
+// // },10)
+
+// // setInterval(function(){
+// //     alert('Xấu như quần què á!')
+// //     console.log('Như con khỉ đột!' + Math.random()) //Math.random giúp viết ra chuỗi random. Nếu không có Math.random thì sẽ trả ra thông báo trên cùng một dòng trong tab consolog
+// // },10)
+
+
+// // var a = 10000*10000
+// // alert(a);
+
+// // var a=1;
+// // var b=2;
+// // var c=3;
+
+// // alert(a*b*c)
+ 
+// // if(a>b){
+// //     alert('àdgadfs')
+// // }
+// // else{
+// //     alert('adadf')
+// // }
+
+// var a=7;
+// var b=6;
+
+// var c= a%b; //chia lấy dư
+//  var d= a**b;
+//  var e=a**b
+//  a++
+// alert(a)
+// //toán hạng ++ __ 
+// //tiền tố đúng trước toán hạng là tiền tố 
+// //hậu tố  đứng sau toán hạng được gọi là hậu tố
+// //++a sẽ được gọi là tiền tố 
+// //a++ sẽ được gọi là hậu tố
+// //tiền tố hậu tố khá giống nhé nó vẫn tăng lên một đơn vị và giảm một đơn vị giống ý nhau thôi
+
+// var f=10;
+
+// console.log(++f);
+// //Việc 1 +a sẽ hiểu nôn na là a=a+1;
+// // Viêc 3 nó sẽ trả kết quả của a sau khi được tính
+// // trong trường hợp trừ đi môt đơn vị nó cx tương tự tôi nhé.
+
+// var i = --f;
+// console.log('output'+''+ i);
+
+// // Về hậu tố thì cũng khác nhé
+//  var g = f--;
+//  console.log('output'+''+ g+''+'sẽ bằng bao nhiêu nhé??? Tôi đó á??');
+//  // viêc 1 g sẽ copy a tức là bây giờ g=6;
+// //Việc 1 +a sẽ hiểu nôn na là a=a+1;
+// //Việc 3 nó vẫn sẽ trả ra a copy đó nhél
+
+// //Toán tử gán
+
+// var x=1;
+
+// x=x+2;
+
+// console.log(x);
+
+// var firstname ='Luan';
+// var lastname='Le';
+
+// console.log(firstname+lastname);
+
+
+// //Toán tử so sánh
+
+// var o =1;
+// var p=2;
+
+// if(o!=p){
+//     console.log('Suất sắc nhé')
+// }else{
+//  console.log('Ngu mà làm màu')
+// }
+
+// //Bolean
+//  var x=4;
+//  var y=3;
+
+//  var xy =x<y;
+//  console.log(xy);
+//  //Nhớ nhé nếu cứ so sáng lớn bé bằng là cỉu bolean True False nhé .
+//  //If và Else
+
+// //  var t=6;
+// //  var g=4;
+// // if(false){
+// //     alert('Đúng nhé mày!')
+// // }else{
+// //     alert('Không đúng làm sai hết tào lao hết sức');
+// // }
+
+// //Khi ở dạng IF ELSE nó sẽ chuyển sang dạng Bolean nhé.
+// fullname='Lê Trương Thành Luân'
+// lastname='Lê'
+// firstname='Luân'
+
+// //Trong js sẽ có 6 giá trị 
+// // false
+// // '' ""
+// //Undefine
+// //NAN
+// //0
+// //Null
+// // Khi chuyển sang dạng bolean nểu chuyển  sang các giá trị này nó sẽ là False nhé 
+// //Còn không nó sẽ là True
+
+
+// //Toán tử Logics
+// //&& và 
+// // || hoặc 
+// // ! Nott
+// fullname=1
+// lastname=2
+// firstname=3
+
+// if(a>0 && b>0){ // Có nghĩa là nếu a lớn 0 và b cũng lớn hơn 0 thì sẽ in ra kết quá đó nhé phải băt buộc đúng hết nhé thì mới hiện ra kết quả nhé cái này giống Python Nếu và một trong hai sai thì nó sai nhé.
+//     console.log('Chính sát');
+// }
+// //Symbol 
+//  var id = Symbol('id') //
+//  var id1 =Symbol('id')//
+//  console.log(id==id1); // tuy nhiên đăc cùng Symbol nhưng nó lại báo ra là false bởi vì nó phụ thuộc tính di nhất nhé em.
+
+//  //Function
+
+//  var myFunction =function(){
+//     alert('Xin chào tất cả mọi người những con người có tư duy không học mà vẫn muốn có ăn!');
+//  }
+//  myFunction();//ban đầu những đoạn code bên trong function nó sẽ không được chạy nó chỉ chạy khi mình khai báo function()
+//  //những function này được gọi là function tự định nghĩa.
+//  //Object Type
+
+//  var MyObject ={
+//     name:'Luan',
+//     age:16,
+//     address:'Bình Định'
+//  }
+
+//  console.log(MyObject);
+//  // cái này nó có thể chứa nhiều loại đối tượng nhé 
+//  var MyArray =[
+//     {
+//         name:'Luan',
+//     age:16,
+//     address:'Bình Định'
+//     },{
+//         name:'Luan',
+//     age:16,
+//     address:'Bình Định'
+//     },{
+//         name:'Luan',
+//     age:16,
+//     address:'Bình Định'
+//     }
+//  ]
+//  console.log(MyArray);
+
+//  var Myarray =['Luân Lê' ,'Lực Phan','Nguyễn Văn Nhật']
+  
+//  console.log(Myarray)
+
+//  //NumberType
+
+//  g=1;
+//  h=2;
+//  i=3;
+
+//  console.log(typeof MyArray) //đê kiểm tra cỉu dữ liệu số xem nó thuộc cỉu dữ liệu nào á thì ta dùng typeof
+
+//  //Toán tử so sánh
+//  //===
+//  //!==
+
+//  var a=1;
+//  var b=1;
+//  console.log(a!==b)// kiểu ===  nó so sánh tuyệt đối hơn ý 
+// //Câu điều kiện và phép so sánh.
+//  var c=3;
+//  var d=4;
+// var result =c>d && c==d
+// console.log(result);
+// if(c>d){
+//     console.log('A<B')
+// }
+// else{
+//     console.log('Như cái quần què')
+// }
+
+// //chuỗi
+// //gạch chéo ngược (\) backlask.
+// //cách tạo chuỗi
+// //cách 1
+// //sẽ có hai cách
+// var full = new String('Luân');
+// alert( full.length); 
+
+// //backslask  in js
+// //findindex giúp bạn có thế tìm đc vị trí của một ký tự trọng hàng hàng chuỗi  nhiều ký tụ
+// //Làm việc với chuỗi
+// var MyString='Làm việc với chuỗi';
+// console.log(MyString.lastIndexOf('việc')) // Nếu vị trí ký tụ không nằm trọng chuỗi đó thì hiện là -1 trên tab console.
+
+//Mảng
+//  var Language =[
+//     'JavaScript',
+//     'PHP',
+//     'Laravel',
+//  ]
+//  var Language1 =[
+//     'JavaScript',
+//     'PHP',
+//     'Laravel',
+//  ]
+//  //Mảng sẽ chứa các phần tử nhé.
+// //Nhớ về array thì chỉ mục sẽ đánh tự động 
+//  console.log(Language)
+//  console.log(Array.isArray(Language))//check thử xem đây có phải là mảng hay k
+//  //Làm việc với mảng (Array)
+//  //ToString
+//  console.log(    typeof   Language.toString())//Chuyển mảng sang string.
+//  console.log(Language.join('')) //chuyển array thành chuỗi
+//  console.log(Language.pop()) //Xóa element cuối mảng và trả về phần tử đã xóa
+//  console.log(Language)
+//  console.log(Language.push('Reactjs','JavaScript','.Net')) //thêm một hoặc nhiểu phần tử cuối mảng  và sẽ trả độ dài mới của mảng
+//  console.log(Language)
+//  console.log(Language.shift()) //Xóa phần tủ ở đầu mảng và trả đọ dài mới của đầu mảng
+//  console.log(Language)
+//  console.log(Language.unshift('Khùng', 'Điên'))//Thêm một hoặc nhiều phẩn tử của mảng và trả về độ dài mới của mảng .
+//  console.log(Language)
+//  Language.splice(1,1,'Mách')//Có thể xóa, cắt và chèn một phần tử vào mảng và trả về độ dài mới của mảng 
+//  //chuyền chỉ mục để hàm splicinh đặc phần tử vào vị trí đầu tiền khi hàm nó chạy nó sẽ đặc vị trí vào vị trí 1
+//  //tham số thứ nhất sẽ đặc con trỏ vào đâu còn tham số thứ hai sẽ đặc trỏ vào phần tử và xóa vào phân tử đó.
+//  //Mách này sẽ đc thêm nếu phần tử kia bị xóa và Mách sẽ được thêm vào tại vị trí.
+//  console.log(Language)
+//  //Concat nó sẽ giúp bạn nối các array lại với nhau.
+// console.log(Language.concat(Language1)) // concat sẽ hợp nhất hai mảng vào mảng hợp nhất
+// //Slice 
+// console.log(Language.slice(1)) // slide dùng để cắt một vài element
+// console.log(Language.slice(0) )// nó sẽ thành copy mảng.
+// //slice cũng có thể giúp truyền số âm để có thê lấy ra phần tử cuối mảng
+// //Slice có giúp chúng ta có thể lấy xuôi hoặc lấy ngươc tức là lấy từ âm hay dương đều được hết
+// //Hàm
+// //-Hàm sẽ không thi khi chỉ định nghĩa 
+// //Hàm chỉ được thực thi khi được gọi vào
+// //-có thể nhận được tham số
+// //chỉ trả về một giá trị
+
+// function Diologue(){
+//     //code của hàm nó sẽ được thực thi
+//     alert('Hello các bạn nhé')
+// }
+// Diologue();
+// //call() dùng để gọi hàm
+// //Tham số trong hàm
+// function  write(message, message2,message3){
+//     console.log( message)
+//     if(message){
+//         console.log(message)
+//     }
+//     if(message2){
+//         console.log(message2)
+//     }
+//     if(message3){
+//         console.log(message3)
+//     }
+//     console.log( message2)
+//     console.log( message3)
+// }
+// write('Nứng', 'Khùng','Điên');// kiểu như là mình sẽ truyền message vào tham số message
+// //khi gọi và truyèn vào  được gọi là đối số
+// //Khi định nghĩa được gọi là tham số
+// //Kiểu dữ liệu không giới hạn nhé e chỉ cấn đúng cú pháp là được
+// //Return  
+
+// var isConfilm =confirm('Khùng điên')
+
+// console.log(isConfilm);
+
+// function Cong(a,b){
+//     return {a,b}
+// }
+// var result = Cong(8,8)
+
+// console.log(result);//Trong một hàm mà không return mà không trả về giá trị thì nó sẽ trả về undefine
+// //Return có thể trả về bất cứ kiểu dữ liệu gì á
+// //Hiểu hơn về Function
+// //Định nghĩa hàm trong hàm
+// function Show(){
+//     function Show1(){
+//         console.log('dsadd')
+//     }
+//     Show1();
+// }
+// Show();
+
+
+// //Các loại function
+// //1 .Declaration function
+// //2 Expression Function
+
+// var showw =function(){
+
+// }
+// //EX sẽ được gán vào một biến
+// //pollfill
+// //Object trong javaScript
+// var info ={
+//     name:'Luân',
+//     Class:'pnv24A'
+// };
+
+// var Key ='Class';
+// info.email ='Luan@gmail.com'// cú pháp thêm một key value sau khi object nó được tạo
+// info['ten-dem'] ='Trương Thành'// cú pháp thêm một key value sau khi object nó được tạo trong trường hợp key không đúng với quy tắc đặc tên key
+// console.log(info)
+// //lấy dữ liệu ra từ mảng đầu tiên tạ phải lấy đúng tê key của dữ liệu đó
+// console.log(info[Key]);
+
+//object contructor
+// function User (firstname , lastname, avatar) {
+//     this.firstname =firstname ;
+//     this.lastname =lastname;//phướng thức để truyền vào tham số 
+//     this.avatar =avatar;
+//     this.getName =function (){
+//         return `${this.firstname} ${this.lastname}`//KHI mình gọi đối tượng nào  sẽ là đối tượng đó
+//     }
+// }//đây là bản thuyết kế đối tượng
+//  var author =new User('Luan','Le','Avatar');
+//  var user =new User('Nhat','Nguyen','Avatar');
+// User.prototype.className ='sds'//không có prototype thì nó mới hiểu được nếu  không có thì sẽ undifine
+//  author.class ='PNV24A'
+//  user.comment='Xinh xuất sắc'//
+//  console.log(author.className); //Đây là cách để xem lại thuyết kế từ đối tượng đc khỏi tạo trong prototype 
+//  console.log(user);
+//Đối tương Date
+//  var date = new Date();// nó có thể truy suất vào phương thức và thuộc tính  qua đó có thẻ lấy ra được giá trị thơi gian
+//  console.log(  date ) // nó sẽ trả ra thời gian hiện tại ra trên tab Console
+// //Nó sẽ không truy suất và date bởi vì nó không phải thuộc dang date nên không thể nào truy suất được phải bắc buộc là kiểu Date mới có thể truy suất đươc
+// var year = date.getFullYear();  //sẽ lấy ra năm
+// var month =date.getMonth()+1;
+// var day =date.getDate();
+// console.log(`${day}/${month}/${year}`)
+//Câu lệnh rẽ nhánh -if-else
+// var date=2;
+// if(date ===2){
+//     console.log('SFGDSDFNFSFD')
+// }
+// else if(date ===3){
+//     console.log('SFG')
+// }
+// else if(date ===3){
+//     console.log('SFGDSDFN')
+// }
+//Câu lệnh rẽ nhánh -Swicth
+// var date=1;//Swicj có  nghĩa là trong trường hợp
+// switch (date) {
+//     case 1:
+//         console.log('Tháng1')
+//         break;
+//     case 2:
+//         console.log('Tháng2')
+//         break;
+//     default:
+//         console.log('Khùng')
+//         break;
+// }
+
+//Toán tử ba ngôi
+// var course ={
+//     name:'JavaScript',
+//     coin:300
+
+// }
+// if(course.coin >0){
+//     console.log(`${course.coin} Coin`)
+// }else{
+//     console.log('Miễn phí')
+// }
+
+//Vòng lặp
+//for lặp với điều kiện đúng
+//for/in lặp qua key của đối tượng
+//for/of lặp qua value của đối tượng
+//white -lặp khi điều kiện đúng
+//do/while -Lặp ít nhất 1 lần , sau đó lặp khi điều kiện nó đúng hiểu như chạy 1 lần nhưng những lần sau điều kiện phải đúng thì mới chạy khong thì dừng vòng lặp
+///Vòng lặp for
+
+// var MyArray =[
+//     'java',
+//     'Python',
+//     'PHP',
+//     'Reactjs',
+//     'Laravel',
+//     'Ruby'
+// ]
+// // console.log(MyArray)
+// var arrayLength =MyArray.length; //dùng arrayLenth nó sẽ tự động tăng lên một phần tử khi thêm dữ liệu mới và trog mảng
+// for(var i = 0; i <arrayLength; i++){
+//     //code
+//     console.log(MyArray[i])
+// }
+//var i=1 nó chạy di nhất một lần để tạo và giá trị i =1
+//For/in dùng để lấy ra key của mamg
+
+// var MyInfo ={
+//     name:'Thành Luân',
+//     age:20,
+//     address:'Bình Định'
+// }
+// for (var key in MyInfo){
+//     //code
+//     console.log(MyInfo[key])
+// }
+
+// var Class=[
+//     'PNV23A',
+//     'PNV23B',
+//     'PNV24A',
+//     'PNV24B',
+//     'PNV25A',
+//     'PNV25B'
+// ]
+//  for(var key in Class){
+//     console.log(Class[key])
+//  }
+
+//  var MyString ='Luân lê'
+//  for(var key in MyString){
+//     console.log(MyString[key])
+//  }
+//For of 
+ var Khung =[
+    'a',
+    'b','c','d','e'
+ ]
+ for( var value of Khung){
+    console.log(value)
+ }
+
+ var Dien={
+    name:'Bình thường',
+    name1:'Tam thân'
+ }
+console.log(Object.keys(Dien))
+console.log(Object.values(Dien)) //Object.keys nó sẽ giúp vòng for of có thể lấy được key và value trong object
+ for(var value of Object.values(Dien)){
+    console.log(value)
+ }//
